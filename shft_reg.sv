5760cd3abb494ab7fed22e91bbc4a1345135ce99
Tej Patel <tej.patel025@gmail.com>
2026-08-19 21:13:21 -0400

    fix(shft_reg.sv): change function priority and enables

--- shft_reg.sv
+++ shft_reg.sv
@@ -1,13 +1,34 @@
 module shft_reg(
+    input  logic       clk,
     input  logic       scl,
-    input  logic       sdaIn,
     input  logic       reset,
+    input  logic       sdaIn,
     input  logic [7:0] txData,
     input  logic       load,
     input  logic       en,
     output logic [7:0] rxData,
     output logic       sdaOut
 );
-    // write states
-    // shift bits when writing 0 (or ack bit?). otherwise, output z
+    logic [7:0] data;
+    logic       sclPrev;
+
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) begin
+            data <= '0;
+            sclPrev <= '1;
+        end
+        else begin
+            sclPrev <= scl;
+
+            if (load) begin
+                    data <= txData;
+                end
+            else if (en == '1 && scl == '1 && sclPrev == '0) begin
+                data <= {data[6:0], sdaIn};
+            end
+        end
+    end
+
+    assign sdaOut = data[7];
+    assign rxData = data;
 endmodule
